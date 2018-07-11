4babe24f7ebc80247d4a61d33ac335611f390ec3
bashment <bashysarah@gmail.com>
2018-07-10 23:04:36 -0500

    adding existing class exercise copied from week3 working folder

---
+++ HobbyBook_working.py
@@ -0,0 +1,16 @@
+# @TODO: Your code here
+# create dictionary of my info
+myinfo = {"name": "Sarah", 
+          "age": "41",
+          "hobbies": ["reading", "photography", "playing with my kids", "snuggling my animals"],
+          "wake-up": {"Mon" : 6, "Wed" : 8, "Sunday" : 10}}
+
+#print results
+print (f'Hello, I am {myinfo["name"]} and I am {myinfo["age"]}')
+print (f'I have {len (myinfo["hobbies"])} hobbies')
+print (f'On the weekend I get to wake up at {myinfo["wake-up"]["Sunday"]}')
+
+
+
+
+    
